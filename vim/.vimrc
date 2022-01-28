cc5e95b698f9f7383b65acaf5fce5699670da411
Lucho <cizara@gmail.com>
2022-01-28 11:12:54 +0100

    move configuration to GNU stow

---
+++ vim/.vimrc
@@ -0,0 +1,6 @@
+runtime! /usr/share/vim/vimfiles/archlinux.vim
+syntax on
+set tabstop=2
+set softtabstop=0 noexpandtab
+set shiftwidth=2
+set tabstop=4 softtabstop=0 expandtab shiftwidth=2 smarttab
---
+++ waybar/.config/waybar/modules/weather.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Updates weather forecast
+
+ip r | while read Def _ IP _; do
+	case $Def in
+		default)
+			ping -q -w 1 -c 1 "$IP" >/dev/null || exit
+
+			break ;;
+	esac
+done
+
+#location="$1"; [ -z "$location" ] || location="$location+"
+#curl -s wttr.in/shiraz?format=4
+#pkill -RTMIN+5 i3blocks && notify-send "🌞 Weather forecast updated."
+ansiweather -l "Ciudad Autónoma de Buenos Aires" -u metric -s true -f 0 -a false | cut -d' ' -f7-9
+#clweather --latitude 29.6068 --longitude 52.5376 -t #API KEY
+#echo 'not now'
